7e6ddf4a995af547640e90aaa2d4c72bbd769daf
Marek Kawałek <marek.kawalek@sofomo.eu>
2016-05-13 12:26:40 +0200

    Added Readme #2.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple ToDo app
+
+This is yet-another example of how a ToDo app can be built using React and other tools.
+
+## How to use
+
+```
+npm install
+PORT=xxx npm start
+```
+
+and then navigate to
+
+```
+http://localhost:xxx/
+```
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <h3>TodoApp Component.</h3>
+            </div>
+        );
+    }
+}
