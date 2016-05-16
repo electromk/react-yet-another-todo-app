8bb2530965f18fecc703b2c36718f95463e41f24
Marek Kawałek <marek.kawalek@sofomo.eu>
2016-05-16 11:02:28 +0200

    Added simple build option.

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ if (TARGET === 'start' || !TARGET) {
             host: process.env.HOST,
             port: process.env.PORT
         },
-        devtool: 'cheap-module-eval-source-map',
+        devtool: 'eval',
         plugins: [
             new webpack.HotModuleReplacementPlugin()
         ]
@@ -81,7 +81,20 @@ if (TARGET === 'start' || !TARGET) {
 }
 
 if (TARGET === 'build') {
-    module.exports = merge(common, {});
+    module.exports = merge(common, {
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
 }
 
 if (TARGET === 'test') {
